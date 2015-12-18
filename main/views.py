fc0e21188dbca0a12cdcf1734f4992b6b8185f47
Alexandr Azizyan <alex.r.azizian@gmail.com>
2015-12-19 02:02:27 +0400

    getip works

--- main/views.py
+++ main/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+from django.http import HttpResponse
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+
+    return HttpResponse(ip, status=200)
