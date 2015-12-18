8745b2c2338d0973eba874e47536cd4d0a2b1d60
Alexandr Azizyan <alex.r.azizian@gmail.com>
2015-12-19 02:11:41 +0400

    getip works

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from main import views
 
 urlpatterns = patterns('',
-                       url(r'^getip/$', views.get_client_ip, name='getip')
-                       )
+    url(r'^getip/$', views.get_client_ip, name='getip')
+)
